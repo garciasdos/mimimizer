f2b45e0226f7844f8289b5b1e41cff86326887df
Diego García <garciasdos@gmail.com>
2021-03-31 23:19:34 +0200

    Move to vercel dir structure

---
+++ api/[...all].js
@@ -0,0 +1,20 @@
+import { HTTPReceiver, LogLevel } from "@slack/bolt";
+import bolt from "../lib/bolt";
+
+const receiver = new HTTPReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  processBeforeResponse: true,
+  endpoints: "/api/slack/events",
+  logLevel: LogLevel.DEBUG,
+});
+
+bolt(receiver);
+
+export default receiver.requestListener;
+
+export const config = {
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
+};
